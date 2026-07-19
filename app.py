730967ae9fa26b92e8f07ddd5e833dcd1421effb
Izaskun <TU_EMAIL_GITHUB>
2026-07-19 21:44:56 +0200

    Conectar informe Sol Ascendente Nodos

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify, send_from_directory
+
 from carta_natal_base import generar_carta_api
 from luna_casa4_casa6 import generar_carta_api as generar_luna_api
+from sol_ascendente_nodos import generar_carta_api as generar_sol_asc_nodos_api
+
 import os
+
 
 app = Flask(__name__)
 
@@ -31,8 +35,9 @@ def generar_carta():
     hora = datos.get("hora")
     lugar = datos.get("lugar")
 
-    lat = datos.get("lat")
-    lon = datos.get("lon")
+    # Wix envía estos campos con estos nombres.
+    lat = datos.get("latitud")
+    lon = datos.get("longitud")
     tz_name = datos.get("tz_name")
 
     opciones = datos.get("opciones", [])
@@ -50,6 +55,8 @@ def generar_carta():
     )
 
     if "opLuna" in opciones:
+        print("Generando informe Luna")
+
         resultado = generar_luna_api(
             nombre,
             fecha,
@@ -59,7 +66,23 @@ def generar_carta():
             lon=lon,
             tz_name=tz_name
         )
+
+    elif "opSolAscNodos" in opciones:
+        print("Generando informe Sol · Ascendente · Nodos")
+
+        resultado = generar_sol_asc_nodos_api(
+            nombre,
+            fecha,
+            hora,
+            lugar,
+            lat=lat,
+            lon=lon,
+            tz_name=tz_name
+        )
+
     else:
+        print("Generando Carta Base")
+
         resultado = generar_carta_api(
             nombre,
             fecha,
