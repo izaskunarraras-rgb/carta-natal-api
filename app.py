c84c6287f070bebf51d33e6375ec44f677e2434d
Izaskun Arraras <izaskun.arraras@gmail.com>
2026-07-28 12:51:01 +0200

    Corregida sintaxis Planetas Sociales

--- app.py
+++ app.py
@@ -95,18 +95,18 @@ def generar_carta():
             tz_name=tz_name
         )
 
-        elif "opSociales" in opciones:
-            print("Generando informe Planetas Sociales")
+    elif "opSociales" in opciones:
+        print("Generando informe Planetas Sociales")
 
-            resultado = generar_planetas_sociales_api(
-                nombre,
-                fecha,
-                hora,
-                lugar,
-                lat=lat,
-                lon=lon,
-                tz_name=tz_name
-            )
+        resultado = generar_planetas_sociales_api(
+            nombre,
+            fecha,
+            hora,
+            lugar,
+            lat=lat,
+            lon=lon,
+            tz_name=tz_name
+        )
 
     else:
         print("Generando Carta Base")
